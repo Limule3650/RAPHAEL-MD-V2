739ebfa3702c9d35f8b6dba8f3d3aca7c2103fdd
Limule3650 <152185817+Limule3650@users.noreply.github.com>
2024-08-03 07:54:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,60 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+process.env.OWNER_NUMBER = process.env.OWNER_NUMBER || '237698581946'
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
+global.port= process.env.PORT || 5000
+global.email = 'mikaelkiyotaka@gmail.com'
+global.github = 'https://github.com/Limule3650/RAPHAEL-MD-V2'
+global.location = 'Africa, Cameroon, Douala'
+global.gurl = 'https://instagram.com/lawliet.kfx' // add your username
+global.sudo = process.env.SUDO || '237698581946'
+global.devs = '237698581946';
+global.website = 'https://github.com/Limule3650/RAPHAEL-MD-V2' //wa.me/+237698581946
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/341a1353ab82acbb7fef0.jpg'
+module.exports = {
+  botname:   process.env.BOT_NAME === undefined ? '*RAPHAEL-MD-V2*' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'LIMULE SOLITARUS' : process.env.OWNER_NAME,
+  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '*RAPHAEL-MD-V2*' : process.env.PACK_INFO.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '*RAPHAEL-MD-V2*' : process.env.PACK_INFO.split(";")[1],
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '229' : process.env.FAKE_COUNTRY_CODE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
+  HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
+  warncount : process.env.WARN_COUNT === undefined ? 4 : process.env.WARN_COUNT,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
+  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
+  BRANCH: 'main', 
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? 'HI Limule-sama, *RAPHAEL-MD-V2* is Alive and operational...' : process.env.ALIVE_MESSAGE,
+  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
+  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
+  HEROKU: {
+    HEROKU: process.env.HEROKU ||false,
+    API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
+    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
+},
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.2' : process.env.VERSION,
+  LANG: process.env.THEME|| 'RAPHAEL-MD-V2',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE,
+  LG: process.env.LG === undefined ? 'false' : process.env.LG,
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
